f0fc5555ae2ad64d8b0804f92240ee8b81e8b725
jju6581 <okhee6018@gmail.com>
2023-09-02 10:36:37 +0900

    feat : 홈 api 추가

--- src/api/movie.ts
+++ src/api/movie.ts
@@ -11,5 +11,9 @@ const REST_API_ADDR = axios.create({
 });
 
 export const homeApi = {
-    
-}
+    nowPlaying : () : Promise<AxiosResponse<NowPlayingData>> => REST_API_ADDR.get("movie/now_playing"),
+    movieDetail : (id:number) : Promise<AxiosResponse<MovieDetailData>> => 
+    REST_API_ADDR.get(`movie/${id}`,{
+        params :{append_to_response : "videos"}
+    }),
+};
